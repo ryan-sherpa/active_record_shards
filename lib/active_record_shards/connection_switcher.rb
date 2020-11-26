25280d113b7a7ac784fe242e2daf479ed2332263
Brian Burns <bburns@zendesk.com>
2020-11-26 10:00:07 +0000

    make on_cx_switch_block backwards compatible

--- lib/active_record_shards/connection_switcher.rb
+++ lib/active_record_shards/connection_switcher.rb
@@ -114,7 +114,9 @@ module ActiveRecordShards
 
     def on_cx_switch_block(which, force: false, construct_ro_scope: nil, &block)
       @disallow_replica ||= 0
-      @disallow_replica += 1 if which == :primary
+      @disallow_replica += 1 if [:primary, :master].include?(which)
+
+      ActiveRecordShards::Deprecation.warn('the `:master` option should be replaced with `:primary`!') if which == :master
 
       switch_to_replica = force || @disallow_replica.zero?
       old_options = current_shard_selection.options
@@ -129,7 +131,7 @@ module ActiveRecordShards
         readonly.scoping(&block)
       end
     ensure
-      @disallow_replica -= 1 if which == :primary
+      @disallow_replica -= 1 if [:primary, :master].include?(which)
       switch_connection(old_options) if old_options
     end
 
