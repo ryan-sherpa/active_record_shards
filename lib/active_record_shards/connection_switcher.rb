4852037f9b23466af4cae26f7328404901fc43fc
Brian Burns <bburns@zendesk.com>
2020-11-26 17:31:02 +0000

    remove check on slave, we no longer use the option

--- lib/active_record_shards/connection_switcher.rb
+++ lib/active_record_shards/connection_switcher.rb
@@ -167,11 +167,6 @@ module ActiveRecordShards
 
     def switch_connection(options)
       if options.any?
-        if options.key?(:slave)
-          ActiveRecordShards::Deprecation.warn('the `:slave` option should be replaced with `:replica`!')
-          options[:replica] ||= options.delete(:slave)
-        end
-
         if options.key?(:replica)
           current_shard_selection.on_replica = options[:replica]
         end
